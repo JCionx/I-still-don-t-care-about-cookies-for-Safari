765dd3910f5ec131cdd68305a0547a8e2d6eb323
JCionx <92257741+JCionx@users.noreply.github.com>
2024-10-20 16:39:41 +0100

    Add files via upload

---
+++ I still don't care about cookiesTests/I_still_don_t_care_about_cookiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_still_don_t_care_about_cookiesTests.swift
+//  I still don't care about cookiesTests
+//
+//  Created by JCionx on 20/10/2024.
+//
+
+import Testing
+@testable import I_still_don_t_care_about_cookies
+
+struct I_still_don_t_care_about_cookiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
